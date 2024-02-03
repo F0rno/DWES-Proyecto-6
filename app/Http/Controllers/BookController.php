7e1d6cf4805c499c5b4233ad45bd2bbe688c6510
F0rno <pablo.fornell.perinan@gmail.com>
2024-02-03 21:57:05 +0100

    internal: search book by name

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,4 +11,32 @@ class BookController extends Controller
     {
         return Book::all();
     }
+
+    public function show($id)
+    {
+        $book = Book::find($id);
+
+        if (!$book) {
+            return response()->json(['error' => 'Book not found by id'], 404);
+        }
+
+        return $book;
+    }
+
+    public function searchByTitle(Request $request)
+    {
+        $searchTerm = $request->input('title');
+
+        if (!$searchTerm) {
+            return response()->json(['error' => 'Search term not provided'], 400);
+        }
+
+        $books = Book::where('title', 'REGEXP', $searchTerm)->get();
+        /*
+        if ($books->isEmpty()) {
+            return response()->json(['error' => 'No books found'], 404);
+        }
+        */
+        return $books;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['namespace' => 'App\Http\Controllers'], function () {
-    Route::apiResource('/descriptions', DescriptionController::class);
-    Route::apiResource('/books', BookController::class);
-});
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/search', [BookController::class, 'searchByTitle']);
+
+Route::get('/descriptions', [DescriptionController::class, 'index']);
+Route::get('/descriptions/{id}', [DescriptionController::class, 'show']);
